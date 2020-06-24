8f0b3b87ccc8610df60f867b5eae23b810f613b2
shubham <shubham@rytangle.com>
2020-06-24 17:57:17 +0530

    first alpha

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# fibo-react
-React SDK for fibo
+# react-native-fibo-react
+
+## Getting started
+
+`$ npm install fibo-react --save`
+
+### Mostly automatic installation
+
+`$ react-native link fibo-react`
+
+## Usage
+```javascript
+import Fiboview from 'fibo-react';
+
+// TODO: What to do with the module?
+Fiboview;
+```
--- index.js
+++ index.js
@@ -1,7 +1,108 @@
-/**
- * @format
- */
+import React, { Component } from 'react';
+import { StyleSheet, AppState } from 'react-native';
+import { WebView } from 'react-native-webview';
 
-import Fiboview from './webView';
+// ...
+export default class Fiboview extends Component {
+    componentDidMount() {
+        // window.addEventListener("click", this.handleClick, false)
+        AppState.addEventListener("change", function (params) {
+            console.log(AppState.currentState); // (active/background) for page changes
+            // (inactive) for ios
+            // TODO: handle page_open, page_close here
+        });
+    }
 
-export {Fiboview};
+    handleClick(e) {
+        let misc = {};
+        try {
+            // click names
+            console.log(e._targetInst.child.memoizedProps);
+            misc.text = e._targetInst.child.memoizedProps || "";
+            // TODO: handle click_event here
+        } catch (error) {
+        }
+        this.webref.injectJavaScript(`fibo.setEvent("click_event", "", { misc: ${misc} })`);
+    }
+
+    handlePageChange(e) {
+        console.log(e);
+        // TODO: handle page_close and page_open
+    }
+
+    render() {
+        console.log(this.props);
+        // TODO: Set gid html as this.props.appid
+        return <WebView
+        ref={(r) => {this.webref = r}}
+        originWhitelist={['*']}
+        url={{ uri: 'http://unireply.com/andy.html' }}
+        source={{ uri: 'http://unireply.com/andy.html' }}
+        style={styles.visible}
+        onError={syntheticEvent => {
+            const { nativeEvent } = syntheticEvent;
+            console.warn('WebView error: ', nativeEvent);
+        }}
+        domStorageEnabled={true}
+        javaScriptCanOpenWindowsAutomatically={true}
+        mixedContentMode={"compatibility"}
+        thirdPartyCookiesEnabled={true}
+        allowsFullscreenVideo={true}
+        allowsInlineMediaPlayback={false}
+        dataDetectorTypes={['all']}
+        directionalLockEnabled={false}
+        geolocationEnabled={false}
+        keyboardDisplayRequiresUserAction={true}
+        allowsBackForwardNavigationGestures={true}
+        allowFileAccess={true}
+        cacheMode={"LOAD_NO_CACHE"}
+        pagingEnabled={true}
+        allowsLinkPreview={true}
+        sharedCookiesEnabled={true}
+        textZoom={100} />;
+    }
+
+    set(name, obj) {
+        console.log("got to set", name, obj);
+        if (!name)
+            return "not recogonized";
+        switch(name) {
+            case "userInfo":
+                this.webref.injectJavaScript(`fibo.setUserInfo(${obj})`);
+                break;
+            case "login":
+                this.webref.injectJavaScript(`fibo.login(${obj})`);
+                break;
+            case "signup":
+                this.webref.injectJavaScript(`fibo.signup(${obj})`);
+                break;
+            case "click_event":
+                this.handleClick(e);
+                break;
+            case "page_open":
+                this.handlePageChange(e);
+                break;
+
+        }
+    }
+}
+
+const styles = StyleSheet.create({
+    visible: {
+        position: "absolute",
+        top: 0,
+        left: 0,
+        height: "100%",
+        width: "100%",
+        backgroundColor: "transparent",
+        flex: 1,
+        zIndex: 999999
+    },
+    hidden: {
+        position: "absolute",
+        height: 0,
+        width: 0,
+        backgroundColor: "transparent",
+        flex: 0,
+    }
+});
