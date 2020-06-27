db9f95e7179da543b41b1f1619cf53c56bdcc779
shubham <shubham@rytangle.com>
2020-06-27 20:34:13 +0530

    page change handling

--- index.js
+++ index.js
@@ -2,14 +2,23 @@ import React, { Component } from 'react';
 import { StyleSheet, AppState } from 'react-native';
 import { WebView } from 'react-native-webview';
 
-// ...
+var currentPage = "", pageTs = "";
+
+/**
+ * Main Fiboview component
+ */
 export default class Fiboview extends Component {
     componentDidMount() {
-        // window.addEventListener("click", this.handleClick, false)
-        AppState.addEventListener("change", function (params) {
-            console.log(AppState.currentState); // (active/background) for page changes
-            // (inactive) for ios
-            // TODO: handle page_open, page_close here
+        AppState.addEventListener("change", (params) => {
+            console.log(params);
+            let state = AppState.currentState;
+            let closeState = ['background', 'inactive'];
+            let openState = ['active'];
+
+            if (closeState.includes(state))
+                this.handlePageClose();
+            else if (openState.includes(state))
+                this.handlePageOpen(currentPage);
         });
     }
 
@@ -20,13 +29,27 @@ export default class Fiboview extends Component {
             console.log(e._targetInst.child.memoizedProps);
             misc.text = e._targetInst.child.memoizedProps || "";
         } catch (error) {
+            if (typeof e === "string") {
+                misc.text = e;
+            }
         }
         this.webref.injectJavaScript(`fibo.setEvent("click_event", "${misc.text}", { misc: ${JSON.stringify(misc)} })`);
     }
 
-    handlePageChange(e) {
-        console.log(e);
-        // TODO: handle page_close and page_open
+    handlePageClose() {
+        if (currentPage) {
+            let duration = 0;
+            if (pageTs && !isNaN(pageTs)) {
+                duration = Date.now() - pageTs;
+            }
+            this.webref.injectJavaScript(`fibo.setEvent("page_close", "${currentPage}", { page: "${currentPage}", duration: ${duration} })`);
+        }
+    }
+
+    handlePageOpen(newPage) {
+        pageTs = Date.now();
+        currentPage = newPage;
+        this.webref.injectJavaScript(`fibo.setEvent("page_open", "${currentPage}", { page: "${currentPage}" })`);
     }
 
     render() {
@@ -39,7 +62,7 @@ export default class Fiboview extends Component {
         originWhitelist={['*']}
         url={{ uri: url }}
         source={{ uri: url }}
-        style={styles.visible}
+        style={styles.hidden}
         injectedJavaScriptBeforeContentLoaded={uiFunc}
         onError={syntheticEvent => {
             const { nativeEvent } = syntheticEvent;
@@ -79,19 +102,20 @@ export default class Fiboview extends Component {
                 this.webref.injectJavaScript(`fibo.signup(${JSON.stringify(val)})`);
                 break;
             case "click_event":
-                this.handleClick(e);
+                this.handleClick(val);
                 break;
             case "page_open":
-                this.handlePageChange(e);
+                this.handlePageClose();
+                this.handlePageOpen(val);
                 break;
-            case "open":
-                this.webref.injectJavaScript(`fibo.open({name: "messenger", type: "open"})`);
-                this.webref.style = styles.visible;
-                break;
-            case "close":
-                this.webref.injectJavaScript(`fibo.open({name: "messenger", type: "close"})`);
-                this.webref.style = styles.hidden;
-                break;
+            // case "open":
+            //     this.webref.injectJavaScript(`fibo.open({name: "messenger", type: "open"})`);
+            //     this.webref.style = styles.visible;
+            //     break;
+            // case "close":
+            //     this.webref.injectJavaScript(`fibo.open({name: "messenger", type: "close"})`);
+            //     this.webref.style = styles.hidden;
+            //     break;
             default:
                 this.webref.injectJavaScript(`fibo.setEvent("${name}", "${val}", ${JSON.stringify(obj)})`);
         }
